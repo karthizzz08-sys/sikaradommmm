0e2c7efeb438a0580bd6d1b97fec30ae062b37f5
karthizzz08-sys <karthizzz08@gmail.com>
2026-04-11 16:41:12 +0530

    map

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,7 +1,7 @@
 import { MapPin } from 'lucide-react';
 
 const GoogleMap = () => {
-  const mapsLink = "https://maps.app.goo.gl/ttyArqidAoVCNsdy5";
+  const mapsLink = "https://www.google.com/maps/place/SIKARA+MAHAL/@10.7837147,79.1492118,3a,75y,19.66h,90t/data=!3m7!1e1!3m5!1sTpAhdhCkOnD9yQkBxkfDfw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D0%26panoid%3DTpAhdhCkOnD9yQkBxkfDfw%26yaw%3D19.659372!7i16384!8i8192!4m14!1m7!3m6!1s0x3baab9a170ce0a67:0xef059c9376cf42f4!2sSIKARA+MAHAL!8m2!3d10.7837618!4d79.1492248!16s%2Fg%2F11n6rl_x1q!3m5!1s0x3baab9a170ce0a67:0xef059c9376cf42f4!8m2!3d10.7837618!4d79.1492248!16s%2Fg%2F11n6rl_x1q!17m2!4m1!1e3!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDQwOC4wIKXMDSoASAFQAw%3D%3D";
 
   return (
     <section id="location" className="py-16 px-4 bg-background">
@@ -15,7 +15,7 @@ const GoogleMap = () => {
 
         <div className="rounded-lg overflow-hidden shadow-lg border border-border">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.5!2d72.5!3d23.07!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1sSikara%20Mahal!2s!5e0!3m2!1sen!2sin!4v1712800000000&q=Sikara+Mahal+Ahmedabad"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3905.123!2d79.1492248!3d10.7837618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baab9a170ce0a67:0xef059c9376cf42f4!2sSIKARA%20MAHAL!5e0!3m2!1sen!2sin!4v1712929200000"
             width="100%"
             height="450"
             style={{ border: 0 }}
